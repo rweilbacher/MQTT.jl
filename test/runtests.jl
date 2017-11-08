f2656556bd344c34394cfd2b488ca6fba22e5e18
Alexander Rohde <rohde.alx@gmail.com>
2017-11-08 16:39:05 +0100

    Simple logging

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,55 +7,57 @@ using Base.Test
 # this could create problems in the future if you want to implement some reconnection for example
 
 function on_msg(topic, payload)
-  println("topic: ", topic, " payload: ", String(payload))
-  @test topic == "abc"
-  @test String(payload)== "qwerty"
+    info("Received message topic: [", topic, "] payload: [", String(payload), "]")
+    @test topic == "abc"
+    @test String(payload)== "qwerty"
 end
 
 function is_out_correct(filename_expected::AbstractString, actual::Channel{UInt8})
-  file = open(filename_expected, "r")
-  correct = true
-  while !eof(file)
-    if read(file, UInt8) != take!(actual)
-      correct = false
-      break
+    file = open(filename_expected, "r")
+    correct = true
+    while !eof(file)
+        if read(file, UInt8) != take!(actual)
+            correct = false
+            break
+        end
     end
-  end
-  if isready(actual)
-    correct = false
-  end
-  return correct
+    if isready(actual)
+        correct = false
+    end
+    return correct
 end
 
 function test()
-  client = Client(on_msg)
+    client = Client(on_msg)
 
-  connect(client, "test.mosquitto.org", client_id="TestID")
-  tfh::TestFileHandler = client.socket
-  @test is_out_correct("data/output/connect.dat", tfh.out_channel)
+    info("Testing connect")
+    connect(client, "test.mosquitto.org", client_id="TestID")
+    tfh::TestFileHandler = client.socket
+    @test is_out_correct("data/output/connect.dat", tfh.out_channel)
 
-  subscribe_async(client, "abc", 0x01, "cba", 0x00)
-  put_from_file(tfh, "data/input/suback.dat")
-  @test is_out_correct("data/output/subreq.dat", tfh.out_channel)
+    info("Testing subscribe")
+    subscribe_async(client, "abc", 0x01, "cba", 0x00)
+    put_from_file(tfh, "data/input/suback.dat")
+    @test is_out_correct("data/output/subreq.dat", tfh.out_channel)
 
-  put_from_file(tfh, "data/input/publish.dat")
+    info("Testing publish")
+    put_from_file(tfh, "data/input/publish.dat")
+    publish_async(client, "test1", b"QOS_0", qos=0x00)
+    @test is_out_correct("data/output/qos0pub.dat", tfh.out_channel)
+    publish_async(client, "test2", b"QOS_1", qos=0x01)
+    put_from_file(tfh, "data/input/puback.dat")
+    @test is_out_correct("data/output/qos1pub.dat", tfh.out_channel)
 
-  publish_async(client, "test1", b"QOS_0", qos=0x00)
-  @test is_out_correct("data/output/qos0pub.dat", tfh.out_channel)
+    info("Testing unsubscribe")
+    unsubscribe_async(client, "abc", "cba")
+    put_from_file(tfh, "data/input/unsuback.dat")
+    @test is_out_correct("data/output/unsubreq.dat", tfh.out_channel)
 
-  publish_async(client, "test2", b"QOS_1", qos=0x01)
-  put_from_file(tfh, "data/input/puback.dat")
-  @test is_out_correct("data/output/qos1pub.dat", tfh.out_channel)
+    info("Testing disconnect")
+    disconnect_async(client)
+    @test is_out_correct("data/output/disco.dat", tfh.out_channel)
 
-  #sleep(10)
-  unsubscribe_async(client, "abc", "cba")
-  put_from_file(tfh, "data/input/unsuback.dat")
-  @test is_out_correct("data/output/unsubreq.dat", tfh.out_channel)
-
-  disconnect_async(client)
-  @test is_out_correct("data/output/disco.dat", tfh.out_channel)
-
-  println("All tests successful!")
+    sleep(0.5)
 end
 
 test()
