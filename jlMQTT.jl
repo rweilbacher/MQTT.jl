d9b5d7f222aef5d23755c2f0e608925fc4be05d3
Alexander Rohde <rohde.alx@gmail.com>
2017-11-08 14:01:00 +0100

    removed old files

--- jlMQTT.jl
+++
@@ -1,8 +0,0 @@
-module jlMQTT
-
-include("types.jl")
-include("client.jl")
-include("consts.jl")
-include("test.jl")
-
-end  # modul jlMQTT
--- test.jl
+++
@@ -1,9 +0,0 @@
-#=function on_msg(topic, msg)
-end
-
-client = connect("127.0.0.1", 1883, on_msg)
-disconnect(client)=#
-a = MQTTMessageInfo(6)
-println(a.mid)
-println(a.published)
-println(a.rc)
